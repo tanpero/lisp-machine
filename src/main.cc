6fdb4164a673882af80f0762bd756547da06ed97
tanpero <camille-dolma@outlook.com>
2024-11-06 19:12:41 +0800

    Update: Add Lexer

---
+++ src/main.cc
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <vector>
+#include <variant>
+#include <memory>
+#include <stack>
+#include <string>
+#include <cctype>
+#include <unordered_map>
+
+enum class TokenType {
+    Number, Symbol, String, OpenParen, CloseParen
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+
+    Token(TokenType type, std::string value) : type(type), value(value) {}
+};
+
+static bool isValidSymbol(char c) {
+    return std::isgraph(c) && c != '(' && c != ')' && c != '"';
+}
+
+class Lexer {
+    size_t position = 0;
+    size_t size;
+
+    std::string source;
+    std::vector<Token> tokens;
+
+    size_t parenLevels = 0;
+
+public:
+    Lexer(std::string source) : source(source), size(source.size()), tokens({}) {}
+
+    std::vector<Token> tokenize();
+
+private:
+
+    Token getNumber() {
+        return { TokenType::Number, readNumber(source, position) };
+    }
+
+    Token getString() {
+        return { TokenType::String, readString(source, position) };
+    }
+
+    Token getIdentifier() {
+        return { TokenType::Symbol, readIdentifier(source, position) };
+    }
+
+private:
+
+    std::string readNumber(const std::string& str, size_t& start) {
+
+        size_t i = start;
+
+        std::string result;
+
+        // 检查负号
+        if (str[i] == '-') {
+            result += '-';
+            ++i;
+        }
+
+        // 读取整数部分
+        while (std::isdigit(str[i])) {
+            result += str[i];
+            ++i;
+        }
+
+        if (str[i] == '.')
+        {
+            result += str[i];
+            ++i;
+        }
+
+        while (std::isdigit(str[i])) {
+            result += str[i];
+            ++i;
+        }
+
+        start = i;
+
+        return result;
+    }
+
+
+    std::string readString(const std::string& str, size_t& start) {
+
+        std::string result;
+        size_t i = start + 1; // 跳过起始的双引号
+
+        while (i < str.length()) {
+            char currentChar = str[i];
+
+            // 检查转义字符
+            if (currentChar == '\\' && i + 1 < str.length()) {
+                char nextChar = str[i + 1];
+                switch (nextChar) {
+                case 'n':
+                    result += '\n';
+                    break;
+                case 't':
+                    result += '\t';
+                    break;
+                default:
+                    result += nextChar;
+                    break;
+                }
+                i += 2; // 跳过转义字符和它后面的字符
+            }
+            else if (currentChar == '\"') {
+                // 遇到结束的双引号，退出循环
+                ++i;
+                break;
+            }
+            else {
+                result += currentChar;
+                ++i;
+            }
+        }
+
+        start = i;
+
+        return result;
+    }
+
+    std::string readIdentifier(const std::string& str, size_t& start) {
+
+        size_t i = start;
+        std::string identifier;
+
+
+        while (isValidSymbol(str[i])) {
+            identifier += str[i];
+            ++i;
+        }
+
+        start = i;
+
+        return identifier;
+    }
+
+};
+
+
+#define current source[position]
+
+std::vector<Token> Lexer::tokenize()
+{
+    while (position < size) {
+        if (std::isblank(current) || current == '\n') {
+            while (std::isblank(current) || current == '\n')
+            {
+                position++;
+            }
+        }
+        else if (current == '(')
+        {
+            tokens.push_back({ TokenType::OpenParen, "("});
+            position++;
+        }
+        else if (current == ')')
+        {
+            tokens.push_back({ TokenType::CloseParen, ")" });
+            position++;
+        }
+        else if (current == '-')
+        {
+            if (std::isdigit(source[position + 1]))
+            {
+                tokens.emplace_back(getNumber());
+            }
+            else
+            {
+                tokens.emplace_back(getIdentifier());
+            }
+        }
+        else if (isValidSymbol(current) && !isdigit(current))
+        {
+            tokens.emplace_back(getIdentifier());
+        }
+        else if (std::isdigit(current))
+        {
+            tokens.emplace_back(getNumber());
+        }
+        else if (current == '"')
+        {
+            tokens.emplace_back(getString());
+        }
+        else
+        {
+            std::cerr << "Unexpected character: \"" << current << "\"\n\n";
+            exit(-1);
+        }
+    }
+    return tokens;
+}
+
+#undef current
+
+int main() {
+
+    auto tokens = Lexer(R"((define f x (x))
+    (+ "Hello" -1.5)
+)").tokenize();
+
+    for (auto t : tokens) std::cout << t.value << "\n";
+
+    return 0;
+}
